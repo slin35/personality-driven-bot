63d5e087da1b65dd52bff3a3e7ebd53b6dff7b83
slin35 <slin35@calpoly.edu>
2021-05-23 01:19:58 -0700

    added aiml rules

--- testbot.py
+++ testbot.py
@@ -1,6 +1,8 @@
 import sys
 import socket
 import time
+import aiml
+import os
 
 class TestBot:
     def __init__(self, server, port, channel, nickname):
@@ -22,24 +24,47 @@ class TestBot:
         return resp
 
     def send(self, msg):
-        self.irc.send(bytes(f"PRIVMSG {self.channel} {msg}\n", "UTF-8"))
+        self.irc.send(bytes(f"PRIVMSG {self.channel} :{msg}\n", "UTF-8"))
 
     def start(self):
         self.connect()
 
+        self.setup_kernel()
+
         while True:
-            text = self.get_response()
-            print(text)
+            msg = self.get_response()
+            self.process_utterance(msg)
+            
+    def setup_kernel(self):
+        self.kernel = aiml.Kernel()
 
-            if "hello" in text:
-                self.send("hello!")
+        if os.path.isfile("bot_brain.brn"):
+            self.kernel.bootstrap(brainFile = "bot_brain.brn")
+        else:
+            self.kernel.bootstrap(learnFiles="std-startup.xml", commands="load aiml all")
+            self.kernel.saveBrain("bot_brain.brn")
 
-            if "die" in text:
+    def process_utterance(self, msg):
+        # message format <nickname_of_sender>!<hostname> PRIVMSG <channel> :<message>
+        print(msg)
+        if msg.find("PRIVMSG") != -1:
+            user = msg.split('!', 1)[0][1:]
+            channel = msg.split('PRIVMSG', 1)[1].split(':', 1)[0] or ''
+            userMsg = msg.split('PRIVMSG', 1)[1].split(':', 1)[1] or ''
+
+            if f"{self.nickname}: die" in userMsg:
                 self.send("Bye!")
+                self.irc.send(bytes(f"QUIT\n", "UTF-8"))
                 self.irc.shutdown(socket.SHUT_RDWR)
                 self.irc.close()
                 sys.exit()
-
+            elif "list" in userMsg:
+                self.irc.send(bytes(f"NAMES {self.channel}\n", "UTF-8"))
+                names = self.get_response().split(":")[2].strip('\r\n')
+                self.send(names)
+            else:               
+                response = self.kernel.respond(userMsg)
+                self.send(response)
 
 
 def main():
